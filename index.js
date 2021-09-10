864165e32f43c21ae88cb056ef6a12328096fb57
Vico <vinicomelo@gmail.com>
2021-09-09 23:28:32 -0400

    updated user settings

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-var bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(express.json());
-const mongoDbUrl = process.env.DB_URL
+const mongoDbUrl = process.env.DB_URL;
 mongoose.connect(mongoDbUrl);
+
 mongoose.Promise = global.Promise;
 
-mongoose.connect(mongoDbUrl, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+mongoose
+  .connect(mongoDbUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
 
-
-
-require('./routes/taco.routes.js')(app)
+require("./routes/taco.routes.js")(app);
 app.listen(3000);
